1a3721ae473d73edc837059d54a318a1cafd5e0c
Mohamed Baaatour <mohamedbaatourx@gmail.com>
2023-04-14 18:23:56 +0100

    Initial commit

---
+++ Desktop/jsxcheck/src/Component/Profile/FullName.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FullName = () => {
+  return (
+    <div> Full Name : Mohamed Baatour</div>
+  )
+}
+
+export default FullName
---
+++ Desktop/jsxcheck/src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Mohamed from  '../../Images/Mohamed.png'
+
+const ProfilePhoto = () => {
+  return (
+      <div >
+          <img className='M1' src={Mohamed} alt="Pic 1" />
+    </div>
+  )
+}
+
+export default ProfilePhoto
